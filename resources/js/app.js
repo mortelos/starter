6551323121fdab1bddd268fda7c367756de50208
Nathan Jansen <info@nathanjansen.nl>
2026-06-03 13:31:33 +0200

    Fix Livewire ESM import path

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
-import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm.js';
 
 // Livewire bundles Alpine; expose it for debugging and ad-hoc directives.
 window.Alpine = Alpine;
